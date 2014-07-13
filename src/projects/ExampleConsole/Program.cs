e80e04fb9fc1e518a66f3d324e0255fc77e74e3c
Dave Baumann <dave.baumann@route2solutions.com>
2014-07-13 05:16:58 -0500

    Adding Example projects to test with.

---
+++ src/projects/ExampleConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Press any key to exit");
+            Console.WriteLine(string.Empty);
+            Console.Read();
+        }
+    }
+}
---
+++ src/projects/ExampleWeb/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Configuration = ConfigurationManager.AppSettings["mykey"];
+            return View();
+        }
+
+    }
+}
